f9e7b4aa23035777c02a705f1cae4205b376f79a
raghuyallapu-dot <ramyallapu3@gmail.com>
2026-01-09 11:33:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function joinGym() {
+  alert("Welcome to PowerFit Gym! 💪");
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  alert("Thank you! We will contact you soon.");
+  let userName = prompt(`enter a number`);
+  console.log(userName);
+}
